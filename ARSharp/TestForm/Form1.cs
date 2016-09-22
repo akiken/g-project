bc7d802c7a9576e5269d6844825b514e421f360e
akiken <akiken.nit@gmail.com>
2016-09-22 13:44:10 +0900

    ARSharp PMXモデルを表示できるように修正

--- ARSharp/TestForm/Form1.cs
+++ ARSharp/TestForm/Form1.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using MMF.Controls.Forms;
+using MMF.DeviceManager;
+using MMF.Grid;
+using MMF.Kinect;
+using MMF.Matricies.Camera.CameraMotion;
+using MMF.Model.Controller;
+using MMF.Model.PMX;
+using SlimDX;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +18,62 @@ using System.Windows.Forms;
 
 namespace TestForm
 {
-    public partial class Form1 : Form
+    public partial class Form1 : D2DSupportedRenderForm
     {
+        TransformController Controller;
+        PMXModel Model;
+        float t = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            WorldSpace.AddDrawableGroup(new ControllerDrawableGroup(1, "Controller", RenderContext));
+            KeyPreview = true;
+            Controller = new TransformController(RenderContext, ScreenContext.HitChekcer);
+            WorldSpace.AddResource(Controller, "Controller");
+
+            ScreenContext.CameraMotionProvider = new BasicCameraControllerMotionProvider(this, this);
+            BasicGrid gird = new BasicGrid();
+            gird.Visibility = true;
+            gird.Load(RenderContext);
+
+            ScreenContext.WorldSpace.AddResource(gird);
+
+            Model = PMXModelWithPhysics.OpenLoad(@"..\..\..\model\初音ミク2.1Cstyle\初音ミクver.2.1Cstyle.pmx", RenderContext);
+            Model.Transformer.Position = new Vector3(0, 0, 0);
+            WorldSpace.AddResource(Model);
+            //Form1.Controller.setTargetModel(Model);
+            //Form1.Controller.Type = TransformController.TransformType.All;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            e.Cancel = false;
+        }
+
+        protected override void RenderSprite()
+        {
+
+        }
+
+        public override void OnUpdated()
+        {
+            base.OnUpdated();
+        }
+
+        protected override void OnPresented()
+        {
+            base.OnPresented();
+            RenderContext.LightManager.Position = new Vector3((float)Math.Cos(t), 0, (float)Math.Sin(t)) * 10f;
+            t += 0.0001f;
+        }
+
+        
     }
 }
--- ARSharp/TestForm/Program.cs
+++ ARSharp/TestForm/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using MMF;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TestForm.Properties;
 
 namespace TestForm
 {
@@ -14,9 +18,30 @@ namespace TestForm
         [STAThread]
         static void Main()
         {
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += Application_ThreadException;
+            Thread.GetDomain().UnhandledException += Application_UnhandledException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Form1 form = new Form1();
+            MessagePump.Run(form);
+            Settings.Default.Save();
+        }
+
+        private static void Application_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            StreamWriter writer = new StreamWriter("log.txt");
+            writer.Write(e.ExceptionObject.ToString());
+            writer.Flush();
+            writer.Close();
+        }
+
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            StreamWriter writer = new StreamWriter("log.txt");
+            writer.Write(e.Exception.ToString());
+            writer.Flush();
+            writer.Close();
         }
     }
 }
